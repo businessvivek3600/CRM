45523b746f2275f4b9cb0ac641943b234fbe0eae
Rajput diksha singh <dikshatouchwood@gmail.com>
2024-12-11 15:33:01 +0530

    RESOLVE CHANGES

---
+++ lib/features/auth/dashboard/components/drawer_fragment/leads_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LeadsScreen extends StatefulWidget {
+  const LeadsScreen({super.key});
+
+  @override
+  State<LeadsScreen> createState() => _LeadsScreenState();
+}
+
+class _LeadsScreenState extends State<LeadsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
