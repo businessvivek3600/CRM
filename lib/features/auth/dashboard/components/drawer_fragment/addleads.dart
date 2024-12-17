9e12d9570c5e30832798f351861289118fab74a3
Rajput diksha singh <dikshatouchwood@gmail.com>
2024-12-17 18:06:02 +0530

    lEADS-addnew leads not Completeds

---
+++ lib/features/auth/dashboard/components/drawer_fragment/addleads.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Addleads extends StatefulWidget {
+  const Addleads({super.key});
+
+  @override
+  State<Addleads> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Addleads> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
